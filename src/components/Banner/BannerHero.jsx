271fb02e6018ac6d8312d20f28cc9320376c31e2
Ramon <ramonlirani@hotmail.com>
2023-07-12 17:28:28 -0300

    feat: bannerHero finished

--- src/components/Banner/BannerHero.jsx
+++ src/components/Banner/BannerHero.jsx
@@ -4,8 +4,52 @@ import Banner from '../../assets/bannerHero.png';
 
 const BannerHero = () => {
   return (
-    <div>
-      <img src={Banner} alt="BannerHero" />
+    <div className="bannerHeroContainer">
+      <img className="bannerHero" src={Banner} alt="BannerHero" />
+      <div className="inforBanner">
+        <p
+          style={{
+            fontSize: '80px',
+            fontFamily: 'Poppins, sans-serif',
+            fontWeight: 600,
+          }}
+        >
+          Tênis Casual
+        </p>
+        <p
+          style={{
+            fontSize: '60px',
+            color: '#CF5D00',
+            fontFamily: 'Poppins, sans-serif',
+            fontWeight: 400,
+            opacity: 0.8,
+          }}
+        >
+          Masculino
+        </p>
+        <p
+          style={{
+            fontSize: '24px',
+            color: '#383838',
+            fontFamily: 'Poppins, sans-serif',
+            fontWeight: 500,
+            opacity: 0.6,
+          }}
+        >
+          A partir de
+        </p>
+        <p
+          style={{
+            fontSize: '50px',
+            color: '#383838',
+            fontFamily: 'Poppins, sans-serif',
+            fontWeight: 700,
+          }}
+        >
+          R$ 79,99
+        </p>
+        <button className="btnBanner">Aproveite</button>
+      </div>
     </div>
   );
 };
